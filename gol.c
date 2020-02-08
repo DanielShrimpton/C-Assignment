cd44027b302314cbadee3ffb7f0938a2c92325c4
Daniel Shrimpton <44175578+DanielShrimpton@users.noreply.github.com>
2020-02-08 15:49:30 +0000

    read_in_file reads file and creates cells in array No dynamic size assignment yet

--- gol.c
+++ gol.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 #include "gol.h"
 
 void main()
 {
-    struct universe *v;
+    struct universe *v[24][512];
     FILE *fp;
     fp = fopen("glider.txt", "r");
     read_in_file(fp, v);
--- gol.h
+++ gol.h
@@ -1,5 +1,7 @@
-struct universe {
-/*Put some appropriate things here*/
+struct universe
+{
+    /*Put some appropriate things here*/
+    char cells[24][512];
 };
 
 /*Do not modify the next seven lines*/
@@ -7,15 +9,18 @@ void read_in_file(FILE *infile, struct universe *u);
 void write_out_file(FILE *outfile, struct universe *u);
 int is_alive(struct universe *u, int column, int row);
 int will_be_alive(struct universe *u, int column, int row);
-int will_be_alive_torus(struct universe *u,  int column, int row);
+int will_be_alive_torus(struct universe *u, int column, int row);
 void evolve(struct universe *u, int (*rule)(struct universe *u, int column, int row));
 void print_statistics(struct universe *u);
 /*You can modify after this line again*/
 
-void read_in_file(FILE *infile, struct universe *u) {
-    char ch[100];
-    if (ch) {
-        printf(ch);
+void read_in_file(FILE *infile, struct universe *u)
+{
+    int i=0, j=0;
+    while (fgets(u[0].cells[i], 512, infile)) {
+        u[0].cells[i][strlen(u[0].cells[i])-1] = '\0';
+        i++;
     }
-    printf("%s", fgets(ch, 50, infile));
+
+    for (j=0; j<i; j++) printf("%d. %s\n", j, u[0].cells[j]);
 }
