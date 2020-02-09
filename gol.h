8a87d63ff84c7b4eacb87e6c6a25a9ed7259cd0e
Daniel Shrimpton <44175578+DanielShrimpton@users.noreply.github.com>
2020-02-09 19:01:09 +0000

    Used dynamic memory allocation

--- gol.h
+++ gol.h
@@ -1,7 +1,7 @@
 struct universe
 {
     /*Put some appropriate things here*/
-    char cells[24][512];
+    char **cells;
 };
 
 /*Do not modify the next seven lines*/
@@ -16,11 +16,34 @@ void print_statistics(struct universe *u);
 
 void read_in_file(FILE *infile, struct universe *u)
 {
-    int i=0, j=0;
-    while (fgets(u -> cells[i], 512, infile)) {
-        u -> cells[i][strlen(u -> cells[i])-1] = '\0';
+    u->cells = (char **)calloc(1, sizeof(char *));
+    for (int i = 0; i < 1; i++)
+    {
+        u->cells[i] = (char *)calloc(512, sizeof(char));
+    }
+    // arr[0] = (char*)calloc(512, sizeof(char));
+    int i = 0, j = 0;
+    int size;
+    bool first = true;
+    while (fgets(u->cells[i], 512, infile))
+    {
+        size = strlen(u->cells[i]);
+        if (first)
+        {
+            u->cells[i] = (char *)realloc(u->cells[i], size * sizeof(char));
+            first = false;
+        }
+        u->cells[i][strlen(u->cells[i]) - 1] = '\0';
+        u->cells = (char **)realloc(u->cells, (i + 2) * sizeof(char *));
+        u->cells[i + 1] = (char *)calloc(size, sizeof(char));
         i++;
     }
 
-    for (j=0; j<i; j++) printf("%d. %s\n", j, u[0].cells[j]);
+    u->cells[i] = (char *)realloc(u->cells[i], 2*sizeof(char));
+    u->cells[i] = '\0';
+
+    // for (j = 0; j < i; j++)
+    // {
+    //     printf("%d. %s\n", j, u->cells[j]);
+    // }
 }
