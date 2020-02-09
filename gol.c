f616d0699b3c7ec70cadcd075750d6be001ed4e3
Daniel Shrimpton <44175578+DanielShrimpton@users.noreply.github.com>
2020-02-09 19:00:49 +0000

    Used dynamic memory allocation and got lengths

--- gol.c
+++ gol.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "gol.h"
 
 void main()
@@ -9,4 +11,15 @@ void main()
     fp = fopen("glider.txt", "r");
     read_in_file(fp, &v);
     fclose(fp);
+    int r = 0, c = 0;
+    // Why doesn't this one work?? 
+    // while (v.cells[0][c] != '\n') 
+    while ((v.cells[0][c] == '.') | (v.cells[0][c] == '*'))
+    {
+        c++;
+    }
+    while ((v.cells[r] != NULL) && (*v.cells[r] != '\0'))
+    {
+        r++;
+    }
 }
