fb3fd0822afc6565759647051500b377abc3822a
DanielShrimpton <44175578+DanielShrimpton@users.noreply.github.com>
2020-02-14 15:05:36 +0000

    Error handling for writing files

--- gameoflife.c
+++ gameoflife.c
@@ -61,14 +61,16 @@ int main(int argc, char *argv[])
     // }
     if (in)
     {
-        if (access(in_name, F_OK) != -1) {
+        if (access(in_name, F_OK) != -1)
+        {
             fp = fopen(in_name, "r");
             read_in_file(fp, &v);
-        } else {
+        }
+        else
+        {
             fprintf(stderr, "gameoflife: the file '%s' does not exist\n", in_name);
             exit(1);
         }
-        
     }
     else
     {
@@ -92,30 +94,27 @@ int main(int argc, char *argv[])
     }
     if (out_)
     {
-        if (access(out, F_OK) == -1) {
+        if (access(out, F_OK) == -1)
+        {
             op = fopen(out, "w");
             write_out_file(op, &v);
-        } else {
-            char ans[1];
-            while ((ans[0] != 'y') | (ans[0] != 'n'))
+            return 0;
+        }
+        else
+        {
+            if (strstr(out, ".txt\0") != NULL)
             {
-                printf("%c", ans[0]);
-                if (ans[0] != '\n') {
-                    printf("The file '%s' already exists, are you sure you want to replace it? (y/n): ", out);
-                    fgets(ans, 2, stdin);
-                }
+                op = fopen(out, "w");
+                write_out_file(op, &v);
+                return 0;
             }
-            switch (ans[0]) {
-                case 'y':
-                    op = fopen(out, "w");
-                    write_out_file(op, &v);
-                    break;
-                case 'n':
-                    write_out_file(stdout, &v);
-                    break;
+            else
+            {
+                write_out_file(stdout, &v);
+                fprintf(stderr, "gameoflife: cannot overwrite non-text files. Printed to screen instead\n");
+                exit(1);
             }
         }
-        
     }
     else
     {
